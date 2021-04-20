7a3433c00bb4e3b383e64dd64c9939eb81d938cd
Rob Ranf <rob@emiyaconsulting.com>
2021-04-20 14:34:16 -0700

    init

---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,15 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Robert Ranf on 4/20/21.
+//
+
+import Foundation
+
+struct CoinManager {
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = (Bundle.main.infoDictionary?["API_KEY"]as?String)!
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+}
